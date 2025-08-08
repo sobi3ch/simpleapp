00e74dda9886c0e2e9448781c06759f9e6b2b3f0
Piotr Sobieszczański <piotr.sobieszczanski@gmail.com>
2025-08-08 06:21:11 +0200

    refactor for one reqeust function

--- index.php
+++ index.php
@@ -9,33 +9,18 @@ echo '<a href="/about   ">About</a> | ';
 echo '<a href="/contact">Contact</a>';
 echo '</nav>';
 
-function home()
+function renderPage($path)
 {
-    echo '<h1>Home</h1>';
-    url();
-}
+    $titles = [
+        '' => 'Home',
+        'about' => 'About',
+        'contact' => 'Contact'
+    ];
 
-function about()
-{
-    echo '<h1>About</h1>';
-    url();
-}
-
-function contact()
-{
-    echo '<h1>Contact</h1>';
+    $title = $titles[$path] ?? 'Home';
+    echo "<h1>$title</h1>";
     url();
 }
 
 $page = $_GET['page'] ?? '';
-switch ($page) {
-    case 'about':
-        about();
-        break;
-    case 'contact':
-        contact();
-        break;
-    default:
-        home();
-        break;
-}
+renderPage($page);
